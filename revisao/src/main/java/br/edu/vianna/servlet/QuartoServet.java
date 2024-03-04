fa745121511b6ea4fbad5d4b7f921e5f5f7a00a5
Rafael <2001campos.rafael@gmail.com>
2024-03-04 20:50:24 -0300

    aula 04/03 pt2

--- revisao/src/main/java/br/edu/vianna/servlet/QuartoServet.java
+++ revisao/src/main/java/br/edu/vianna/servlet/QuartoServet.java
@@ -1,4 +1,46 @@
 package br.edu.vianna.servlet;
 
-public class QuartoServet {
+import br.edu.vianna.model.Calculadora;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("quarto")
+public class QuartoServet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resposta(req, resp);
+    }
+
+    private void resposta(HttpServletRequest req, HttpServletResponse resp) {
+
+        try {
+            String v1 = req.getParameter("cpValor1");
+            String v2 = req.getParameter("cpValor2");
+            String btn = req.getParameter("btn");
+
+            Calculadora c = new Calculadora();
+            c.setValor1(Integer.parseInt(v1));
+            c.setValor2(Integer.parseInt(v2));
+            c.setOperador(btn);
+
+            RequestDispatcher dr= req.getRequestDispatcher("respostaForward.jsp");
+            dr.forward(req,resp);
+        } catch (ServletException e) {
+            System.out.println("deu ruim");
+        } catch (IOException e) {
+            System.out.println("deu ruim");
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resposta(req, resp);
+    }
 }
